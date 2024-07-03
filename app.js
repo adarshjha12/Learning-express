51d1eaac6781395a4c7bd5fd415caef50d8fd743
adarsh <jhaa50872@gmail.com>
2024-07-03 17:06:12 +0530

    express started

---
+++ app.js
@@ -0,0 +1,16 @@
+const express  = require('express')
+const port = 8000
+let app = express()
+
+app.get('/', (req, res) =>{
+    res.send('welcome adarsh')
+})
+
+app.get('/about', (req, res) =>{
+    res.send('welcome adarsh on about page')
+})
+
+app.listen(port, () =>{
+    console.log('running on port');
+})
+
