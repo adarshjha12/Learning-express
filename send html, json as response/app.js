6c30a6bc39a60fac043b91f0add529094d24a91b
adarsh <jhaa50872@gmail.com>
2024-07-05 11:53:20 +0530

    changes

--- send html, json as response/app.js
+++ send html, json as response/app.js
@@ -1,18 +1,21 @@
 let fs = require('fs')
 let express = require('express')
-
-let file = fs.readFileSync('index.html', 'utf-8')
 let app = express()
+let path = require('path')
+let file = fs.readFileSync('index.html', 'utf-8')
+
 
 // we are using three methods to send data to the user
 // in 1st method we used readfile function from file system
 // in 2nd method we use json data inside res.end in /api endpoint.
 // in 3rd method we use express.ststic function to send file to client
 
+//1st
 app.get('/', (req, res) =>{
     res.send(file)
 })
 
+//2nd
 app.get('/api', (req, res) =>{
     res.send({
         name: 'adarsh',
@@ -20,9 +23,8 @@ app.get('/api', (req, res) =>{
     })
 })
 
-app.get('/new', (req, res) =>{
-    res.send()
-})
+//3rd
+
 
 app.listen(8000, () =>{
     console.log('running');
