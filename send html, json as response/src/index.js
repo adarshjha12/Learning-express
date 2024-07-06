6ac49450f87f3b2b37f940dc53d11d5f0f4ef41e
adarsh <jhaa50872@gmail.com>
2024-07-06 16:30:35 +0530

    package added

--- send html, json as response/src/index.js
+++ send html, json as response/src/index.js
@@ -1,8 +1,7 @@
-let fs = require('fs')
-let express = require('express')
-let app = express()
-let path = require('path')
-let file = fs.readFileSync('../public/a.html', 'utf-8')
+// let fs = require('fs')
+// let express = require('express')
+// let app = express()
+// let path = require('path')
 
 
 // we are using three methods to send data to the user
@@ -10,23 +9,52 @@ let file = fs.readFileSync('../public/a.html', 'utf-8')
 // in 2nd method we use json data inside res.end in /api endpoint.
 // in 3rd method we use express.ststic function to send file to client
 
+
 //1st
 // app.get('/', (req, res) =>{
-//     res.send(file)
+//     res.send('hello')
 // })
 
 //2nd
-app.get('/api', (req, res) =>{
-    res.send({
-        name: 'adarsh',
-        class: null
-    })
-})
+// app.get('/api', (req, res) =>{
+//     res.send({
+//         name: 'adarsh',
+//         class: null
+//     })
+// })
 
 //3rd
-let staticPath = path.join(__dirname, '../public')
-app.use(express.static(staticPath))
+// let staticPath = path.join(__dirname, '../public')
+// app.use(express.static(staticPath))
 
+
+// app.listen(8000, () =>{
+//     console.log('running');
+//     // console.log(staticPath);
+// 👍👍
+// })
+
+// above example is for routing in express and sending html page to client and json data with three methods
+
+
+
+// template engines in node js 🖥️🛜
+
+let fs = require('fs')
+let express = require('express')
+let app = express()
+let path = require('path')
+
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+app.get('/', (req, res) =>{
+    res.send('hello')
+})
 
 app.listen(8000, () =>{
     console.log('running');
@@ -34,6 +62,3 @@ app.listen(8000, () =>{
  
 
 })
-
-// above example is for routing in express and sending html page to client and json data with three methods
-
